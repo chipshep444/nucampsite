6f16adc916d54127959316e543640da3ec980aca
Charles Shepherd <chipshep007@yahoo.com>
2021-02-06 12:05:30 -0500

    020621 1205pm

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,13 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+
+    $('#reserveButton').click(function() {
+        $('#reserveModal').modal("show")
+    });
+
+    $('#loginButton').click(function() {
+        $('#loginModal').modal("show")
+    });
+
 });
